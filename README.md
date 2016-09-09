366beb04a6eaca22d83c35e5935932462799162d
Zhenghao Yin <Zhenghao Yin>
2016-09-09 22:32:05 +0800

    add mzi

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Hello, World
+# Introduction
+
+This is a reposity aimed to design Photonics Integrated Circuts more easily with gdspy. It is more friendly to waveguide optical structure and non-source device design. 
+
+# Installation
+
+- install Python and pip 
+
+    sudo apt-get install python3 python-pip
+
+- use pip install gdspy
+    
+    sudo pip istall -t gdspy
+
+- clone this reposity
+
+
+# Usage
--- bend.py
+++ bend.py
@@ -1,6 +1,12 @@
 import numpy
 import gdspy
 
+def mix(list_com,layer):
+	tot = gdspy.Path(1,(0,0))
+	for i in list_com:
+		tot = gdspy.fast_boolean(i, tot, 'or', layer=layer)
+	return tot
+
 def s_bend(origin, length, height, width, layer):
 	r = (length**2 + height**2)/height/4.0
 	theta = numpy.arcsin(length/r/2.0)
@@ -21,14 +27,30 @@ def branch(origin, length, height, width, layer):
 	path2.segment(length-r, '+x', layer=layer).turn(r,-numpy.pi/2.0,layer=layer)
 	return gdspy.fast_boolean(path1, path2, 'or', layer=layer)
 
-def taper():
+def taper(origin, length, width1, width2, layer):
+	#a = 2*(width1-width2), b = -3.0*a/2.0, c = width1
+	path = gdspy.Path(width1,origin)
+	path.parametric(lambda t: (t,0), final_width = lambda t: 2*(width1-width2)*t**3-3*(width1-width2)*t**2+width1, 
+		layer=layer)
+	return path
+
+def mzi(origin, length, height, width, layer):
+	spec={'width': width, 'layer': layer}
+	origin1=(origin[0]+height/2.0,origin[1])
+	origin2=(origin[0]+length,origin[1])
+	sp1 = splitter(origin, height/2.0, height, **spec)
+	sp2 = splitter(origin2, height/2.0, height, **spec).rotate(numpy.pi,center=origin2)
+	path=gdspy.Path(width,origin1,number_of_paths=2,distance=height)
+	path.segment(length-height,'+x')
+	return mix([sp1,sp2,path],layer)#gdspy.fast_boolean(sp1, sp2, 'or', layer=layer)
+
+
 cell = gdspy.Cell('test')
 
 #sb = s_bend((0,0),0.1,4,4,3)
 spec = {'width':0.8, 'layer':3}
 
-sp = splitter((0,0),length=10,height=20,**spec)
-
-cell.add(sp)
+#sp = splitter((0,0),length=10,height=20,**spec)
+cell.add(mzi((0,0),10,4,0.1,3))
 
 gdspy.LayoutViewer()
